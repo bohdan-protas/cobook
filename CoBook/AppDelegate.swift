0e90053330c5a729a0495784ded00e2721aa0ea4
protas <bo7pro@gmail.com>
2020-02-07 10:47:13 +0200

    Initial commit

---
+++ CoBook/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  CoBook
+//
+//  Created by protas on 2/7/20.
+//  Copyright © 2020 CoBook. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
