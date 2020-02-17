ebd32e9f08dbf603b7ad3fa3517c024f34673010
Bohdan <Protas>
2020-02-17 15:02:12 +0200

    Custom button component

---
+++ CoBook/Views/Custom/CustomButton.swift
@@ -0,0 +1,21 @@
+//
+//  CustomButton.swift
+//  CoBook
+//
+//  Created by protas on 2/17/20.
+//  Copyright © 2020 CoBook. All rights reserved.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
