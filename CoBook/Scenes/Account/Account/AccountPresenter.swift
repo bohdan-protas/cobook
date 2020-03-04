52cc58dd2b55bbeb9e3089eb8a82218e11e5646c
Bohdan <Protas>
2020-03-04 13:34:30 +0200

    Profile tabbar setup

---
+++ CoBook/Scenes/Account/Account/AccountPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  AccountPresenter.swift
+//  CoBook
+//
+//  Created by protas on 3/4/20.
+//  Copyright © 2020 CoBook. All rights reserved.
+//
+
+import Foundation
---
+++ CoBook/Views/NavigationControllers/AccountNavigationController.swift
@@ -0,0 +1,30 @@
+//
+//  AccountNavigationController.swift
+//  CoBook
+//
+//  Created by protas on 3/4/20.
+//  Copyright © 2020 CoBook. All rights reserved.
+//
+
+import UIKit
+
+class AccountNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
