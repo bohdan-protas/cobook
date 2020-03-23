3b0e65d54c4c76b67268902059effdc044be8e5d
Bohdan <Protas>
2020-03-23 03:13:37 +0200

    Personal card UI elements setup

---
+++ CoBook/UI/Views/Cells/GetInTouchTableViewCell/GetInTouchTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GetInTouchTableViewCell.swift
+//  CoBook
+//
+//  Created by protas on 3/23/20.
+//  Copyright © 2020 CoBook. All rights reserved.
+//
+
+import UIKit
+
+class GetInTouchTableViewCell: UITableViewCell {
+
+    @IBOutlet var telephoneButton: DesignableButton!
+    @IBOutlet var emailButton: DesignableButton!
+
+    @IBAction func telephoneButtonTapped(_ sender: UIButton) {
+    }
+
+    @IBAction func emailButtonTapped(_ sender: UIButton) {
+    }
+
+    
+}
---
+++ CoBook/UI/Views/EditCardView/EditCardView.swift
@@ -0,0 +1,24 @@
+//
+//  EditCardView.swift
+//  CoBook
+//
+//  Created by protas on 3/23/20.
+//  Copyright © 2020 CoBook. All rights reserved.
+//
+
+import UIKit
+
+class EditCardView: BaseFromNibView {
+
+    @IBOutlet var editButton: LoaderButton!
+    var onEditTapped: (() -> Void)?
+
+    override func getNib() -> UINib {
+        return EditCardView.nib
+    }
+
+    @IBAction func editButtonTapped(_ sender: UIButton) {
+        onEditTapped?()
+    }
+
+}
