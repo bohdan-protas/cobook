5066353a5514c4f13ee48d7b650b397e772c5ce9
Bohdan <Protas>
2020-04-26 21:30:06 +0300

    Add checkbox table view cell

---
+++ CoBook/UI/Views/Cells/CheckboxTableViewCell/CheckboxTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CheckboxTableViewCell.swift
+//  CoBook
+//
+//  Created by protas on 4/26/20.
+//  Copyright © 2020 CoBook. All rights reserved.
+//
+
+import UIKit
+
+class CheckboxTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
