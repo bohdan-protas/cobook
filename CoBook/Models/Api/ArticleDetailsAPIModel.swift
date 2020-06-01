8999b255f3af9a10fca7a5102fb842729f21de1d
Bohdan <Protas>
2020-06-01 03:08:40 +0300

    Article telephone parse fix

--- CoBook/Models/Api/ArticleDetailsAPIModel.swift
+++ CoBook/Models/Api/ArticleDetailsAPIModel.swift
@@ -16,7 +16,7 @@ struct ArticleDetailsAPIModel: Decodable {
     var createdAt: Date?
     var photos: [FileDataApiModel]?
     var album: AlbumPreviewApiModel?
-    var cardInfo: CardPreviewApiModel?
+    var cardInfo: ArticleCardPreviewApiModel?
     var isSaved: Bool?
     var viewsCount: String?
 
@@ -33,4 +33,29 @@ struct ArticleDetailsAPIModel: Decodable {
         case isSaved = "is_saved"
     }
 
+}
+
+struct ArticleCardPreviewApiModel: Codable {
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case type
+        case cardCreator = "created_by"
+        case avatar
+        case telephone = "contact_telephone"
+        case email
+        case practiceType = "practice_type"
+        case company
+        case isSaved = "is_saved"
+    }
+
+    var id: Int
+    var type: CardType
+    var cardCreator: CardCreatorApiModel?
+    var avatar: FileDataApiModel?
+    var telephone: TelephoneApiModel?
+    var email: EmailApiModel?
+    var practiceType: PracticeTypeApiModel?
+    var company: CompanyApiModel?
+    var isSaved: Bool?
 }
