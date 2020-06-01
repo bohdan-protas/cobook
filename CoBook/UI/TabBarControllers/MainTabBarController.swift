97d4ada963656ca6783ebd463217661f25e8a5a2
Bohdan <Protas>
2020-06-01 16:06:33 +0300

    Localization update

--- CoBook/UI/TabBarControllers/MainTabBarController.swift
+++ CoBook/UI/TabBarControllers/MainTabBarController.swift
@@ -17,13 +17,13 @@ class MainTabBarController: UITabBarController {
         super.viewDidLoad()
 
         let allCardsController: CardsOverviewNavigationController = UIStoryboard.allCards.initiateViewControllerFromType()
-        allCardsController.tabBarItem = UITabBarItem(title: "AllCards".localized, image: #imageLiteral(resourceName: "ic_tabbar_allcards_inactive"), selectedImage: #imageLiteral(resourceName: "ic_tabbar_allcards_active"))
+        allCardsController.tabBarItem = UITabBarItem(title: "Tabbar.allCards.title".localized, image: #imageLiteral(resourceName: "ic_tabbar_allcards_inactive"), selectedImage: #imageLiteral(resourceName: "ic_tabbar_allcards_active"))
 
         let savedContentController: SavedContentNavigationController = UIStoryboard.saved.initiateViewControllerFromType()
-        savedContentController.tabBarItem = UITabBarItem(title: "Saved".localized, image: #imageLiteral(resourceName: "ic_tabbar_saved_unactive"), selectedImage: #imageLiteral(resourceName: "ic_tabbar_saved_active"))
+        savedContentController.tabBarItem = UITabBarItem(title: "Tabbar.saved.title".localized, image: #imageLiteral(resourceName: "ic_tabbar_saved_unactive"), selectedImage: #imageLiteral(resourceName: "ic_tabbar_saved_active"))
 
         let accountController: AccountNavigationController = UIStoryboard.account.initiateViewControllerFromType()
-        accountController.tabBarItem = UITabBarItem(title: "Account".localized, image: #imageLiteral(resourceName: "ic_tabbar_account_inactive"), selectedImage: #imageLiteral(resourceName: "ic_tabbar_account_active"))
+        accountController.tabBarItem = UITabBarItem(title: "Tabbar.account.title".localized, image: #imageLiteral(resourceName: "ic_tabbar_account_inactive"), selectedImage: #imageLiteral(resourceName: "ic_tabbar_account_active"))
 
         self.viewControllers = [allCardsController, savedContentController, accountController]
         self.selectedViewController = allCardsController
