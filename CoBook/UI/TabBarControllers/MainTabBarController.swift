a023ee08c5d737ebbe2526d6678858dfc2d1376c
Bohdan Protas <bo7pro@gmail.com>
2020-06-23 19:46:25 +0300

    Hide unused functionality

--- CoBook/UI/TabBarControllers/MainTabBarController.swift
+++ CoBook/UI/TabBarControllers/MainTabBarController.swift
@@ -26,7 +26,7 @@ class MainTabBarController: UITabBarController {
         accountController.tabBarItem = UITabBarItem(title: "Tabbar.account.title".localized, image: #imageLiteral(resourceName: "ic_tabbar_account_inactive"), selectedImage: #imageLiteral(resourceName: "ic_tabbar_account_active"))
 
         self.viewControllers = [allCardsController, savedContentController, accountController]
-        self.selectedViewController = accountController
+        self.selectedViewController = allCardsController
 
         // If pending dynamic link exists - its time to show it
         if let pendingDynamicLink = AppStorage.State.pendingDynamicLink {
